6dd8beb84d49e2ac451bdf16eab65889093bf659
Reece Hudson <reecehudson10@gmail.com>
2016-06-26 13:59:47 -0400

    lib, test:
    1. Enable ability to add multiple type comparisons as different arguments.
    
    2. Makes the first two arguments, actual and expected, non-optional.  Errors
       are thrown.
    
    Also comes with added tests for these features.
    
    This changes the parameters but does not break the api so semver minor it is.

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ process.env.DEBUG = "ch-arge:index.js";
 
 var charge = require("../lib");
 var assert = require("assert");
+var EE = require("events");
 
 // charge with arg as expected instance of constructor
 assert(charge([], Array));
@@ -31,3 +32,63 @@ assert.equal(true, (new charge.ArgError) instanceof Error);
 
 // argError instanceof ArgError
 assert.equal(true, (new charge.ArgError) instanceof charge.ArgError);
+
+describe("parameters", function () {
+  it ("arg", function () {
+    // required
+    expect(
+      charge.bind(null)
+    ).toThrowError("Requires an actual");
+  });
+
+  it ("arg2...[argn]", function () {
+    // at least one is required
+    expect(
+      charge.bind(null, "", false)
+    ).toThrowError("No type seen");
+
+    expect(
+      charge.bind(null, "")
+    ).toThrowError("No type seen");
+    // exp, args...
+    expect(
+            charge.bind(null, "", Object, Array, "str", "arr")
+          ).not.toThrow();
+    // exp, args... shouldTrhow
+    expect(
+      charge(new EE, Array, "str Str", EE, false)
+    ).toBe(true);
+    // FIXME unrecognized type, should we throw??
+    // exp, arg1, arg2 (unrecogznied type)
+    expect(
+      charge(String(), "Str, str", "blurt", false)
+    ).toBe(true);
+    // exp, arg1 (not it), arg2 (it), arg3 (not it)
+    expect (
+      charge(String(), "Str", "str", Object, false)
+    ).toBe(true);
+    // exp, arg1arg2, arg3arg4
+    expect (
+      charge(null, "Object, obj", "undefined|null", false)
+    ).toBe(true);
+    // negated
+    expect (
+      charge("blah", "!null", false)
+    ).toBe(true);
+  });
+
+  it ("boolean should throw", function () {
+    // default
+    expect(
+      function () {charge("blah", String)}
+    ).toThrow();
+    // true
+    expect(
+      function () {charge("blah", String, true)}
+    ).toThrow();
+    // false
+    expect(
+      charge("blah", String, false)
+    ).toBe(false);
+  });
+});
