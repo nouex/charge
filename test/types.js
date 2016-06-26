ff3078309b295516d55976c9655bf6c7988dbbc1
Reece Hudson <reecehudson10@gmail.com>
2016-06-26 00:33:23 -0400

    lib, test: type test redesign, use RE functions
    1.  This changes the architecture of test/types.js in two major ways:
      1. When testing our check functions, we can now "go deep" and check for as
         many things as we'd like -- instead of just one thing to check for -- by
         creating a namespace <type> the is an Array containing the "things" to test
         using our check functions.
    
         This is a +1 for scalability -- we are now able to add as many things to
         have the check function tested for.
    
    2. Same thing is done with to test our regexps of test/types.js.  But in order
        to do that we had to do 4. (wrap a function around our regexps).  So we
        could use the debug function and maintain consistency.
    
    3. We also added the ability to test input on both check functions and regexp
       functions that should result in false return.
    
    4. In order to stay consistent in our code this changes the current state of
        regexps by wrapping them in a function that returns the search result of the
        first argument.
    
        This also affected a single line in lib/index.js

--- test/types.js
+++ test/types.js
@@ -1,75 +1,189 @@
 "use strict";
 
 var types = require("../lib/types.js");
-var assert = require("assert");
 
 describe("check functions", function () {// TODO counter-test
   it("pass", function () {
-    var args = [
-      {},
-      [],
-      Function,
-      /blah/,
-      new Date,
-      Symbol(),
-      new String(),
-      new Number(),
-      new Boolean(),
-      null,
-      undefined,
-      "blah",
-      18,
-      18,
-      18.5,
-      true
-    ];
-    var nextArg, counter = 0;
+    var goodArgs, badArgs, nextArgs, checkFn;
 
-    assert.equal(args.length, 16);
+    goodArgs = {
+      "Object": [
+        {}
+      ],
+      "Array": [
+        []
+      ],
+      "Function": [
+        Function,
+        function () {}
+      ],
+      "RegExp": [
+        /blah/
+      ],
+      "Date": [
+        new Date
+      ],
+      "Symbol": [
+        Symbol()
+      ],
+      "String": [
+        new String()
+      ],
+      "Number": [
+        new Number()
+      ],
+      "Boolean": [
+        new Boolean()
+      ],
+      "null": [
+        null
+      ],
+      "undefined": [
+        undefined
+      ],
+      "string": [
+        "blah"
+      ],
+      "number": [
+        18
+      ],
+      "integer": [
+        18
+      ],
+      "float": [
+        18.5
+      ],
+      "boolean": [
+        true,
+        false,
+        true
+      ]
+    };
+
+    badArgs = {
+      "Object": [
+        Object.create(null)
+      ],
+      "Array": [
+        {},
+        null,
+        undefined
+      ],
+      "Function": [
+        {},
+        []
+      ],
+      "RegExp": [
+        {},
+        []
+      ],
+      "integer": [
+        3.3
+      ],
+      "float": [
+        3
+      ]
+    };
+
     // keeping it DRY
     for (var type in types) {
       if (!types.hasOwnProperty(type)) continue;
 
-      // console.log(counter++, args[0]);
-      nextArg = args.shift();
-      expect(types[type][1](nextArg)).toBe(true);
+      checkFn = types[type][1];
+      nextArgs = goodArgs[type];
+      for (var arg in nextArgs) {
+        if (!nextArgs.hasOwnProperty(arg)) continue;
+
+        debug("checks", true, type, checkFn, nextArgs[arg]);
+        expect(checkFn(nextArgs[arg])).toBe(true);
+      }
+
+      nextArgs = badArgs[type];
+      for (var arg in nextArgs) {
+        if (!nextArgs.hasOwnProperty(arg)) continue;
+
+        debug("checks", false, type, checkFn, nextArgs[arg]);
+        expect(checkFn(nextArgs[arg])).toBe(false);
+      }
     }
   });
 });
 
 describe("type regexes", function () {
   it ("work approprately", function () {
-    var counter = -1;
-    var args = [
-      ["Object", "object", "Obj", "obj"],
-      ["Array", "array", "Arr", "arr"],
-      ["Function", "function", "Func", "func", "Fn", "fn"],
-      ["RegExp", "regExp", "regexp", "regExp", "regex"],
-      ["Date", "date"],
-      ["Symbol", "symbol", "Sym", "sym"],
-      ["String", "Str"],
-      ["Number", "Num"],
-      ["Boolean", "Bool"],
-      ["null"],
-      ["undefined"],
-      ["string", "str"],
-      ["number", "num"],
-      ["integer", "int"],
-      ["float", "flt"],
-      ["bool", "boolean"]
-    ];
+    var goodStrs = {
+      "Object": ["Object", "object", "Obj", "obj"],
+      "Array": ["Array", "array", "Arr", "arr"],
+      "Function": ["Function", "function", "Func", "func", "Fn", "fn"],
+      "RegExp": ["RegExp", "regExp", "regexp", "regExp", "regex"],
+      "Date": ["Date", "date"],
+      "Symbol": ["Symbol", "symbol", "Sym", "sym"],
+      "String": ["String", "Str"],
+      "Number": ["Number", "Num"],
+      "Boolean": ["Boolean", "Bool"],
+      "null": ["null"],
+      "undefined": ["undefined"],
+      "string": ["string", "str"],
+      "number": ["number", "num"],
+      "integer": ["integer", "int"],
+      "float": ["float", "flt"],
+      "boolean": ["bool", "boolean"]
+    };
 
-    assert.equal(args.length, 16);
+    var badStrs = {
+      "Object": ["", "o"]
+    };
+
+    var strs, str, reFn;
 
     // keeping it DRY
     for (var type in types) {
       if (!types.hasOwnProperty(type)) continue;
 
-      counter += 1;
-      args.shift().forEach(function (nextArg, ind) {
-        expect(types[type][0].test(nextArg)).toBe(true, type,
-          "@args[" + counter + "][" + ind + "]");
-      }, null);
+      reFn = types[type][0];
+      strs = goodStrs[type];
+
+      for (var ind in strs) {
+        if (!strs.hasOwnProperty(ind)) continue;
+
+        str = strs[ind];
+        debug("regexp", true, "<void>", reFn, str);
+        expect(reFn(str) >= 0).toBe(true);// 0 - Infinity
+      }
+
+      strs = badStrs[type];
+      for (var ind in strs) {
+        if (!strs.hasOwnProperty(ind)) continue;
+
+        str = strs[ind];
+        debug("regexp", false, "<void>", reFn, str);
+        expect(reFn(str)).toBeLessThan(0);// -1
+      }
     }
   });
 });
+
+function debug (section, goodArg, expType, checkFn, subj) {
+  var util = require("util");
+  if (process.env.no_debug) return;
+  var com = ",",
+      crlf = "\r\n",
+      sp = "\u0020",
+      arrow = "->",
+      subjString = null != subj && subj.toString !== undefined ?
+                    subj.toString() :
+                    util.inspect(subj, {depth: 1, colors: false}),
+      subjTypeof = typeof subj,
+      checkFnRet = checkFn(subj);
+
+  console.error(
+    section + crlf +
+    (goodArg ? "good" : "bad") + crlf +
+    "type" + sp + arrow + sp + expType + crlf +
+    "fn" + sp + arrow + sp + checkFn.name + crlf +
+    "subj as string" + sp + arrow + sp + subjString + crlf +
+    "typeof subj" + sp + arrow + sp + typeof subj + crlf +
+    "fn result" + sp + arrow + sp + checkFn(subj) + crlf
+    + crlf
+  );
+}
