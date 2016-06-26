7febd48c1eb6ef0e05f8248cf7d29cc4f697c1df
Reece Hudson <reecehudson10@gmail.com>
2016-06-26 12:48:54 -0400

    test: fix errors that throw in index.js
    Two lines where stopping b/c of an AssertionError.  They both had to do with
    the fact that e6c6970 now allows `[]` to be a type `Object`.  I don't know why
    the tests before weren't showing this.

--- test/index.js
+++ test/index.js
@@ -9,11 +9,11 @@ var assert = require("assert");
 // charge with arg as expected instance of constructor
 assert(charge([], Array));
 
-// fail on purpose and throws
-assert.throws(function(){charge([], Object)});
+assert.doesNotThrow(function(){charge([], Object)});
 
-// fail on purpose with return instead of throw
-assert.equal(charge([], Object, false), false);
+assert.equal(charge([], Object, false), true);
+
+assert.equal(charge("", Object, false), false);
 
 // charge with string as second arg
 assert.ok(charge([], "array"));
