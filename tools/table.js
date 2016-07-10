29fa7355d43b60a311e622cab1f8e98526c03ab1
Reece Hudson <reecehudson10@gmail.com>
2016-07-09 23:27:52 -0400

    blah

--- tools/table.js
+++ tools/table.js
@@ -101,6 +101,7 @@ function extractFromFile(filename, sep) {
   i2 = readme.lastIndexOf(sep);
   table = readme.slice(i1 + sep.length, i2);
   debug("extractFromFile(): readme file len: %d", readme.length);
+  debug("readme contents: %s", readme);
   debug("extractFromFile(): index of sep: %d", i1);
   debug("extractFromFile(): last index of sep: %d", i2);
   debug("extractFromFile(): table len: %d", table.length);
